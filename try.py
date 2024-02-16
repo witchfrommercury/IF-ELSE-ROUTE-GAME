29d216f168ab9506b45e234dc65b0d763c9bd459
Kimberly Fabunan <157728066+witchfrommercury@users.noreply.github.com>
2024-02-16 21:26:02 +0800

    Update try.py

--- try.py
+++ try.py
@@ -5,14 +5,43 @@ print("Hello! Welcome to my small game that I created.")
 username = input("Pleade enter your username:")
 print("Username is: " + username)
 
-###
+print(" ")    
 
 a = input( username + "Do you want to enter the game? (yes/no)")
 
-###
-
+print(" ")    
 
 if a == "yes":
     print("Great, let's continue!")
 else:
-    print("Ok, let's stop here.")
+    print("You don't have a choice :p .")
+
+
+print(" ")    
+
+print("You woke up from a dream and the first thing you notice is no one is there. What will you do?")
+
+print(" ")    
+
+print("A. You look at the kitchen.")
+print("B. You go to the bathroom.")
+print("C. You went to sleep again.")
+
+print(" ")    
+
+b = input("Answer:" " ")
+
+print(" ")
+
+if b == "A":
+    print("You went to the kitchen and heard a sound.")
+    print(" ")
+    print("A. Peak thru the ladder.")
+if b == "A":
+    print(" ")
+    c = input("Answer:.")
+if b == "B":
+    print("You went to the bathroom and notice something on the mirror.")
+else:
+    print("PLEASE USE CAPITAL LETTERS.")
+
