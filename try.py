c6373b984195804010a42641dfe3b4c76610e392
Kimberly Fabunan <157728066+witchfrommercury@users.noreply.github.com>
2024-02-16 21:10:03 +0800

    Update try.py

--- try.py
+++ try.py
@@ -5,7 +5,12 @@ print("Hello! Welcome to my small game that I created.")
 username = input("Pleade enter your username:")
 print("Username is: " + username)
 
+###
+
 a = input( username + "Do you want to enter the game? (yes/no)")
+
+###
+
 
 if a == "yes":
     print("Great, let's continue!")
